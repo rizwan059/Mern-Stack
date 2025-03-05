7ffab83522b2ad3ff6ab6e4b3d93a043f9c45327
Kabir-Sial <sial059689@gmail.com>
2025-03-05 21:05:51 +0500

    mern stack react project

---
+++ firstMernReact/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+
+      <h1 className='text-center mt-4'>First Mern Stack Project</h1>
+
+    </>
+  )
+}
+
+export default App
